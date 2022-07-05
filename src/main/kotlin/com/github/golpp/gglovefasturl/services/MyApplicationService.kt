dfcfc5dbb7afb4877527aaea3c06a3d79f28f799
GitHub Action <action@github.com>
2022-07-05 07:11:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/golpp/gglovefasturl/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/golpp/gglovefasturl/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.golpp.gglovefasturl.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.golpp.gglovefasturl.MyBundle
 
 class MyApplicationService {
 
